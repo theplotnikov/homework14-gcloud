81a0a4a435a9a6667439e8bd74f9d7c9bd8e3ff9
plotnikov.s <onlygrach@gmail.com>
2020-12-21 16:24:22 +0500

    new

--- hw14-gcloud.tf
+++ hw14-gcloud.tf
@@ -5,24 +5,40 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "build"
   machine_type = "f1-micro"
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
-
   network_interface {
-    # A default network is created for all GCP projects
     network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
 }
-
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  name                    = "default"
+  auto_create_subnetworks = "true"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "deploy"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "default"
   auto_create_subnetworks = "true"
 }
